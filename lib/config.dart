c77e215eeb173d749f2b39116936f006a0e94730
Timothy Wamalwa <timwamalwa@gmail.com>
2025-06-05 12:24:37 +0300

    Updated api endpoint

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 const NEMIS_URI = "https://student-nemis-dbb3c9etf0bbgqd5.southafricanorth-01.azurewebsites.net/api/students/student-profile";
-const BASE_URL = "https://scms-hecyceeza5anbhd7.southafricanorth-01.azurewebsites.net/api";
+const BASE_URL = "https://eduhub001-bmewcahgdpcyfcce.southafricanorth-01.azurewebsites.net/api";
 const REGISTRATION = "$BASE_URL/auth/register/";
 const LOGIN = "$BASE_URL/auth/login/";
